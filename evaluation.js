ad515d2f5b6816ca7d20cfc0f77b814bc213ece5
Edin Jusupovic <edinjusupovic@hotmail.com>
2017-05-01 05:26:00 +0000

    evaluation added, eval params added, prep for modulation later and more

---
+++ evaluation.js
@@ -0,0 +1,12 @@
+//module.exports = {
+//    eval_db_fields: function(inc){
+//        console.log(this.); 
+//        
+//        
+//        
+//        
+//    }
+//}
+
+
+// all evaluation modules will be moved into here! 
--- routes.js
+++ routes.js
@@ -4,14 +4,28 @@ var express = require('express');
 var router = express.Router(); 
 var Invoice = require('./schema').Invoice; 
 
+// TEMPORARY PLACE FOR EVALUATION MODULES 
+
+    // insert here?
+
+
 // create a test invoice 
 router.post('/', function(req, res, next){
-    if (!req.body.name || !req.body.address ||
-       !req.body.services || !req.body.balanceTotal){
+    
+    // check for missing fields before data insertion 
+    if (!req.body.name || 
+        !req.body.address ||
+        !req.body.services ||
+        !req.body.balanceTotal ||
+        req.body.name.length < 3 ||
+        req.body.address.length < 3 || 
+        req.body.services.length < 3 || 
+        req.body.balanceTotal.length < 3
+       ){
         var err = new Error('invoice_creation_missing-data'); 
-        err.status = 500; 
-        return next(err); 
-    }
+            err.status = 500; 
+                return next(err); 
+        }
     var invoice = new Invoice(req.body); 
     invoice.save(function(err, invoice){
         if (err) return next(err); 
