c2a30f5a450fb77190b97ff6d0eaaf192865a269
Edin Jusupovic <edinjusupovic@hotmail.com>
2017-05-06 08:27:02 +0000

    written out the query system map

--- query/key_verification.js
+++ query/key_verification.js
@@ -9,4 +9,36 @@
             // "name": NAME GOES HERE     
             // } 
 
+        // RESULT ==> 
+
+    // Multiple items are numbered. If the system returns 1 item, that means there is only
+    // one record of that invoice in the database. 
+        // JSON return 
+//  {
+//    matching_number: (0-10,000) // will include the number of matching invoices found
+//    invoices: {
+//        1: {
+//            name: john smith
+//            address: 123 watson street sydney 
+//            services: 1x example service
+//            balanceTotal: 85.00
+//        },
+//        2: {
+//            name: john doe 
+//            address: 321 george street sydney 
+//            services: 1x sample service
+//            balanceTotal: 49.95
+//        }
+//      }
+//    }
+
+
+    // NO RECORDS FOUND RESULT ==> 
+
+//{
+//  "status": "fail",
+//  "message": "database_query-not-found"
+//}
+
+
 
